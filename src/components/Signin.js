c7acb82429ac60be419058f4a432ed0568607ad6
Abimbola Olukoju <abimbolaolukoju@Abimbolas-MacBook-Pro.local>
2020-04-20 21:36:49 +0100

    feature(implement-signin):initial commit

---
+++ src/components/Signin.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+const Signin = () =>{
+
+    return(
+        <>
+        <h1>please sign in</h1>
+        <input type="text" placeholder="username"/>
+        <input type ="password" placeholder="password"/>
+        </>
+    )
+}
